404296b1c814f1c57f446dc8016b3376af9da2bb
JonathanOrtega15 <jonathan.ortegacanul1@gmail.com>
2022-09-23 19:22:55 -0500

    Se implemento el widget kartik grid en el modulo de proyecto

--- backend/views/proyecto/index.php
+++ backend/views/proyecto/index.php
@@ -4,12 +4,31 @@ use common\models\Proyecto;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
 
 
 /** @var yii\web\View $this */
 /** @var backend\models\search\ProyectoSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
+$this -> registerCss("
+.btn-warning {
+    color: #212529;
+    background-color: red;
+    border-color: #ffc107;
+}
+
+.text-dark {
+    color: white !important;
+}
+
+.btn-danger {
+    color: #fff;
+    background-color:green;
+    border-color: green;
+}
+
+");
 
 $this->title = 'Proyectos';
 $this->params['breadcrumbs'][] = $this->title;
@@ -18,18 +37,17 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Proyecto', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+    <?php Pjax::begin();?>
+
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= GridView::widget([
-        'dataProvider' => $dataProvider,
+        'dataProvider'=> $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
+            
+            ['class' => 'kartik\grid\SerialColumn'],
             'id',
             'nombre',
              //'departamento_id',
@@ -43,14 +61,36 @@ $this->params['breadcrumbs'][] = $this->title;
             //'estado_proyecto_id',
             //'created_at',
             //'updated_at',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Proyecto $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                }
-            ],
+
+                [
+                    'class' => ActionColumn::className(),
+                    'urlCreator' => function ($action, Proyecto $model, $key, $index, $column) {
+                        return Url::toRoute([$action, 'id' => $model->id]);
+                    }
+                ],
+            
         ],
-    ]); ?>
+        
+        'pjax' => true,
+        'responsive' => true,
+        'hover' => true,
+        'toggleDataOptions' => [
+            'maxCount' => true,
+            ],
+        'toolbar' => [
+            '{export}',
+            '{toggleData}'
+        ],
+        
+        'panel' => [
+            'heading'=>$this->title,
+            'type'=>'info',
+            'before'=>Html::a(Yii::t('app', 'Crear Proyecto'), ['create'], ['class' => 'btn btn-danger']),
+            'after'=>Html::a('<i class="fas fa-redo"></i> Reset Grid', ['index'], ['class' => 'btn btn-info']),
+            'footer'=>false
+        ],
+
+        ]); ?>
 
 
 </div>
