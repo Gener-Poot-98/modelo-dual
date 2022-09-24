7bed448cfe11ecdb69fe82916879d8920cb93fea
JonathanOrtega15 <jonathan.ortegacanul1@gmail.com>
2022-09-23 19:40:19 -0500

    Se implemento el widget kartik-grid para los modulos faltantes

--- backend/views/ingenieria/index.php
+++ backend/views/ingenieria/index.php
@@ -4,11 +4,31 @@ use common\models\Ingenieria;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
 
 /** @var yii\web\View $this */
 /** @var backend\models\search\IngenieriaSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this -> registerCss("
+.btn-warning {
+    color: #212529;
+    background-color: red;
+    border-color: #ffc107;
+}
+
+.text-dark {
+    color: white !important;
+}
+
+.btn-danger {
+    color: #fff;
+    background-color:green;
+    border-color: green;
+}
+
+");
 
 $this->title = 'Ingenierias';
 $this->params['breadcrumbs'][] = $this->title;
@@ -17,28 +37,50 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Ingenieria', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+<?php Pjax::begin();?>
 
+
+    
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= GridView::widget([
-        'dataProvider' => $dataProvider,
+        'dataProvider'=> $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
+            
+            ['class' => 'kartik\grid\SerialColumn'],
             'id',
             'nombre',
+
             [
                 'class' => ActionColumn::className(),
                 'urlCreator' => function ($action, Ingenieria $model, $key, $index, $column) {
                     return Url::toRoute([$action, 'id' => $model->id]);
-                 }
+                }
             ],
+            
         ],
-    ]); ?>
+        
+        'pjax' => true,
+        'responsive' => true,
+        'hover' => true,
+        'toggleDataOptions' => [
+            'maxCount' => true,
+            ],
+        'toolbar' => [
+            '{export}',
+            '{toggleData}'
+        ],
+        
+        'panel' => [
+            'heading'=>$this->title,
+            'type'=>'info',
+            'before'=>Html::a(Yii::t('app', 'Crear Ingenieria'), ['create'], ['class' => 'btn btn-danger']),
+            'after'=>Html::a('<i class="fas fa-redo"></i> Reset Grid', ['index'], ['class' => 'btn btn-info']),
+            'footer'=>false
+        ],
+
+        ]); ?>
 
 
 </div>
--- backend/views/preregistro/index.php
+++ backend/views/preregistro/index.php
@@ -4,11 +4,31 @@ use common\models\Preregistro;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
 
 /** @var yii\web\View $this */
 /** @var backend\models\search\PreregistroSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this -> registerCss("
+.btn-warning {
+    color: #212529;
+    background-color: red;
+    border-color: #ffc107;
+}
+
+.text-dark {
+    color: white !important;
+}
+
+.btn-danger {
+    color: #fff;
+    background-color:green;
+    border-color: green;
+}
+
+");
 
 $this->title = 'Preregistros';
 $this->params['breadcrumbs'][] = $this->title;
@@ -17,18 +37,17 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Preregistro', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+<?php Pjax::begin();?>
+
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= GridView::widget([
-        'dataProvider' => $dataProvider,
+        'dataProvider'=> $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
+            
+            ['class' => 'kartik\grid\SerialColumn'],
             'id',
             'nombre',
             'matricula',
@@ -44,14 +63,38 @@ $this->params['breadcrumbs'][] = $this->title;
             //'estado_registro_id',
             [ 'label' => 'Estado','attribute' => 'estadoRegistroNombre', 'filter' => $searchModel->getEstadoRegistroNombreList() ],
             //'comentario:ntext',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Preregistro $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
+            
+
+                [
+                    'class' => ActionColumn::className(),
+                    'urlCreator' => function ($action, Preregistro $model, $key, $index, $column) {
+                        return Url::toRoute([$action, 'id' => $model->id]);
+                    }
+                ],
+            
         ],
-    ]); ?>
+        
+        'pjax' => true,
+        'responsive' => true,
+        'hover' => true,
+        'toggleDataOptions' => [
+            'maxCount' => true,
+            ],
+        'toolbar' => [
+            '{export}',
+            '{toggleData}'
+        ],
+        
+        'panel' => [
+            'heading'=>$this->title,
+            'type'=>'info',
+            'before'=>Html::a(Yii::t('app', 'Crear Preregistro'), ['create'], ['class' => 'btn btn-danger']),
+            'after'=>Html::a('<i class="fas fa-redo"></i> Reset Grid', ['index'], ['class' => 'btn btn-info']),
+            'footer'=>false
+        ],
+
+        ]); ?>
+
 
 
 </div>
