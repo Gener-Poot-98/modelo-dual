14f528bb72c3dc3b9dccff493db81803d5d450e5
JonathanOrtega15 <jonathan.ortegacanul1@gmail.com>
2022-10-08 22:53:34 -0500

    se modifico el _form de proyecto

--- backend/views/proyecto/_form.php
+++ backend/views/proyecto/_form.php
@@ -14,88 +14,111 @@ use yii\helpers\Url;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+    <div class="row">
 
-    <?= $form->field($model, 'plan_estudios_id')->dropDownList($model->getPlanEstudiosList(), ['prompt' => 'Seleccione el plan de estudio']) ?>
+        <div class="col-md-6">
+            <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+        </div>
 
-    <?= $form->field($model, 'departamento_id')->dropDownList($model->getDepartamentoList(), ['prompt' => 'Seleccione el departamento']) ?>
+        <div class="col-md-6">
+            <?= $form->field($model, 'plan_estudios_id')->dropDownList($model->getPlanEstudiosList(), ['prompt' => 'Seleccione el plan de estudio']) ?>
+        </div>
 
-    <?= $form->field($model, 'asesor_interno_id')->dropDownList($model->getAsesorInternoList(), ['prompt' => 'Seleccione el asesor interno']) ?>
+        <div class="col-md-6">
+            <?= $form->field($model, 'departamento_id')->dropDownList($model->getDepartamentoList(), ['prompt' => 'Seleccione el departamento']) ?>
+        </div>
 
-    <?= $form->field($model, 'ingenieria_id')->dropDownList($model->getIngenieriasList(), ['prompt' => 'Seleccione la Ingeniería']) ?>
+        <div class="col-md-6">
+            <?= $form->field($model, 'asesor_interno_id')->dropDownList($model->getAsesorInternoList(), ['prompt' => 'Seleccione el asesor interno']) ?>
+        </div>
 
-    <?php
-    echo $form->field($model, 'nombreEstudiante')->widget(Typeahead::classname(), [
-        'options' => ['placeholder' => 'Ingresa el nombre del estudiante...'],
-        'pluginOptions' => ['highlight' => true],
-        'dataset' => [
-            [
-                'datumTokenizer' =>
-                "Bloodhound.tokenizers.obj.whitespace('value')",
-                'display' => 'value',
-                'remote' => [
-                    'url' => Url::to(['proyecto/nombre-list']), 
-                        '?q=%QUERY',
-                    'wildcard' => '%QUERY'
+        <div class="col-md-6">
+            <?= $form->field($model, 'ingenieria_id')->dropDownList($model->getIngenieriasList(), ['prompt' => 'Seleccione la Ingeniería']) ?>
+        </div>
+
+        <div class="col-md-6">
+
+            <?php
+            echo $form->field($model, 'nombreEstudiante')->widget(Typeahead::classname(), [
+                'options' => ['placeholder' => 'Ingresa el nombre del estudiante...'],
+                'pluginOptions' => ['highlight' => true],
+                'dataset' => [
+                    [
+                        'datumTokenizer' =>
+                        "Bloodhound.tokenizers.obj.whitespace('value')",
+                        'display' => 'value',
+                        'remote' => [
+                            'url' => Url::to(['proyecto/nombre-list']),
+                            '?q=%QUERY',
+                            'wildcard' => '%QUERY'
+                        ]
+                    ]
                 ]
-            ]
-        ]
-    ]);
-    ?>
+            ]);
+            ?>
+        </div>
 
-    <?php
-    echo $form->field($model, 'nombreEmpresa')->widget(Typeahead::classname(), [
-        'options' => ['placeholder' => 'Ingresa el nombre de la empresa...'],
-        'pluginOptions' => ['highlight' => true],
-        'dataset' => [
-            [
-                'datumTokenizer' =>
-                "Bloodhound.tokenizers.obj.whitespace('value')",
-                'display' => 'value',
-                'remote' => [
-                    'url' => Url::to(['proyecto/empresa-list']), 
-                        '?q=%QUERY',
-                    'wildcard' => '%QUERY'
+        <div class="col-md-6">
+            <?php
+            echo $form->field($model, 'nombreEmpresa')->widget(Typeahead::classname(), [
+                'options' => ['placeholder' => 'Ingresa el nombre de la empresa...'],
+                'pluginOptions' => ['highlight' => true],
+                'dataset' => [
+                    [
+                        'datumTokenizer' =>
+                        "Bloodhound.tokenizers.obj.whitespace('value')",
+                        'display' => 'value',
+                        'remote' => [
+                            'url' => Url::to(['proyecto/empresa-list']),
+                            '?q=%QUERY',
+                            'wildcard' => '%QUERY'
+                        ]
+                    ]
                 ]
-            ]
-        ]
-    ]);
-    ?>
+            ]);
+            ?>
+        </div>
 
-    <?php
-    echo $form->field($model, 'nombreAsesorExterno')->widget(Typeahead::classname(), [
-        'options' => ['placeholder' => 'Ingresa el nombre del asesor externo...'],
-        'pluginOptions' => ['highlight' => true],
-        'dataset' => [
-            [
-                'datumTokenizer' =>
-                "Bloodhound.tokenizers.obj.whitespace('value')",
-                'display' => 'value',
-                'remote' => [
-                    'url' => Url::to(['proyecto/asesor-externo-list']), 
-                        '?q=%QUERY',
-                    'wildcard' => '%QUERY'
+        <div class="col-md-6">
+            <?php
+            echo $form->field($model, 'nombreAsesorExterno')->widget(Typeahead::classname(), [
+                'options' => ['placeholder' => 'Ingresa el nombre del asesor externo...'],
+                'pluginOptions' => ['highlight' => true],
+                'dataset' => [
+                    [
+                        'datumTokenizer' =>
+                        "Bloodhound.tokenizers.obj.whitespace('value')",
+                        'display' => 'value',
+                        'remote' => [
+                            'url' => Url::to(['proyecto/asesor-externo-list']),
+                            '?q=%QUERY',
+                            'wildcard' => '%QUERY'
+                        ]
+                    ]
                 ]
-            ]
-        ]
-    ]);
-    ?>
+            ]);
+            ?>
+        </div>
 
-    <?= $form->field($model, 'periodo_id')->textInput() ?>
+        <div class="col-md-4">
+            <?= $form->field($model, 'periodo_id')->textInput() ?>
+        </div>
 
-    <?= $form->field($model, 'horas_totales')->textInput() ?>
+        <div class="col-md-4">
+            <?= $form->field($model, 'horas_totales')->textInput() ?>
+        </div>
 
-    <?= $form->field($model, 'estado_proyecto_id')->dropDownList($model->getEstadoProyectosList(), ['prompt' => 'Seleccione el estado del proyecto']) ?>
+        <div class="col-md-4">
+            <?= $form->field($model, 'estado_proyecto_id')->dropDownList($model->getEstadoProyectosList(), ['prompt' => 'Seleccione el estado del proyecto']) ?>
+        </div>
+    </div>
 
     <?= $form->field($model, 'descripcion')->textarea(['rows' => 6]) ?>
-
-
+    
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success btn-lg btn-block']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
 
 </div>
-
-
